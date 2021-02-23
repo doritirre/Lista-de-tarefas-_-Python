4f2f390fe2089232ae9dd549412b7f0ee81a9c71
Dori Tirre <doritirre@gmail.com>
2021-02-23 15:59:21 -0300

    Implementando método __iter__ e o vencimento da tarefa

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Projeto:
     def __init__(self, nome):
         self.nome = nome
         self.tarefas = []
 
-    def add(self, descricao):
-        self.tarefas.append(Tarefa(descricao))
+    def __iter__(self):
+        return self.tarefas.__iter__()
+
+    def add(self, descricao, vencimento=None):
+        self.tarefas.append(Tarefa(descricao, vencimento))
 
     def pendentes(self):
         return [tarefa for tarefa in self.tarefas if not tarefa.feito]
@@ -20,37 +23,48 @@ class Projeto:
 
 
 class Tarefa:
-    def __init__(self, descricao):
+    def __init__(self, descricao, vencimento=None):
         self.descricao = descricao
         self.feito = False
         self.criacao = datetime.now()
+        self.vencimento = vencimento
 
     def concluir(self):
         self.feito = True
 
     def __str__(self):
-        return self.descricao + (' (concluída)' if self.feito else '')
+        status = []
+        if self.feito:
+            status.append('(Concluida)')
+        elif self.vencimento:
+            if datetime.now() > self.vencimento:
+                status.append(('Vencida'))
+            else:
+                dias = (self.vencimento - datetime.now()).days
+                status.append(f' vence em {dias} dias')
+        return f'{self.descricao}' + ''.join(status)
+
 
 def main():
     casa = Projeto('Tarefas de casa')
-    casa.add('Passar roupa')
+    casa.add('Passar roupa', datetime.now())
     casa.add('Lavar prato')
     print(casa)
 
     casa.procurar('Lavar prato').concluir()
-    for tarefa in casa.tarefas:
+    for tarefa in casa:
         print(f'- {tarefa}')
     print(casa)
 
     mercado = Projeto('Compras no mercado')
     mercado.add('Frutas secas')
     mercado.add('Carne')
-    mercado.add('Tomate')
+    mercado.add('Tomate', datetime.now() + timedelta(days=3, seconds=12))
     print(mercado)
 
     comprar_carne = mercado.procurar('Carne')
     comprar_carne.concluir()
-    for tarefa in mercado.tarefas:
+    for tarefa in mercado:
         print(f'- {tarefa}')
     print(mercado)
 
